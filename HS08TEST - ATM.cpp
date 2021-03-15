8cc024adda7c2f085a8448aa15f425f954029d73
Dio Aditya Putra Wardhono <55073908+dioapw@users.noreply.github.com>
2021-03-15 11:19:33 +0700

    Add files via upload

---
+++ HS08TEST - ATM.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+#include<bits/stdc++.h>
+using namespace std;
+
+int main() {
+	int money; float balance, total;
+    cin>>money>>balance;
+	if (money%5==0 && (money+0.50) <= balance)
+	{
+	    total = (balance - money)- 0.50;
+	    cout << fixed << setprecision(2) << total;
+	} else {
+	    cout << fixed << setprecision(2) << balance;
+	}
+	return 0;
+}
